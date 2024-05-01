3af9e059a253c308dfe7ab37b3e6fe568e029ed7
Subhadipsahaa <121369221+Subhadipsahaa@users.noreply.github.com>
2024-05-01 22:50:08 +0530

    Update RevNum.php

--- Task4/RevNum.php
+++ Task4/RevNum.php
@@ -15,27 +15,32 @@
 <body>
     <div class="container mt-5">
         <div class="col-6">
-            <form class="frm" method="post">
+            <form class="frm" method="get">
                 <div class="from-group">
                     <label for="name">Enter The Number :</label>
                     <input type="text" name="name" id="name" class="form-control">
                 </div>
                 <br>
-                <input type="submit" name="ok" class="btn btn-primary" value="Submit">
+                <input type="submit" name="ok" class="btn btn-primary" value="Submit/Reset">
                 <br>
                 <br>
                 <?php
-                if (isset($_POST['name'])) {
-                    $num = $_POST['name'];
-                    $rem = 0;
-                    $rev = null;
-                    while ($num > 0) {
+                if (isset($_GET['ok'])) {
+                    if (isset($_GET['name'])) {
+                        if (is_numeric($_GET['name'])) {
+                            $num = $_GET['name'];
+                            $rem = 0;
+                            $rev = null;
+                            while ($num > 0) {
 
-                        $rem = $num % 10;
-                        $rev = $rev * 10 + $rem;
-                        $num = (int)($num / 10);
+                                $rem = $num % 10;
+                                $rev = $rev * 10 + $rem;
+                                $num = (int)($num / 10);
+                            }
+                            echo "The reverse number is :" . $rev;
+                        } else
+                            echo "Enter a Valid Numeric data";
                     }
-                    echo "The reverse number is :" . $rev;
                 }
                 ?>
             </form>
