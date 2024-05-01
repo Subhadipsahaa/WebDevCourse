a46c5cc5641c8197c673df1d91519fd21608404c
Subhadipsahaa <121369221+Subhadipsahaa@users.noreply.github.com>
2024-05-02 00:06:26 +0530

    update

--- Task4/AddTwoNumber.php
+++ Task4/AddTwoNumber.php
@@ -34,7 +34,7 @@
                             $num1 = $_GET['num1'];
                             $num2 = $_GET['num2'];
                             $sum = $num1 + $num2;
-                            echo "The sum of two numbers is :-" . $sum;
+                            echo "The sum of $num1 and $num2 is :-" . $sum;
                         } else
                             echo "Enter a Valid Numeric data";
                     }
